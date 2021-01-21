39aa90e75828dd582fe16b837e306c4f46c2194e
Jiteshw96 <Jitesh.wadhwa13@gmail.com>
2021-01-21 22:16:10 +0530

    "YoutubePlayerAPI Integrated"

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -1,84 +1,56 @@
 package com.example.videoplayer
 
-import android.annotation.SuppressLint
-import android.net.Uri
+
 import android.os.Bundle
-import android.util.SparseArray
-import androidx.appcompat.app.AppCompatActivity
-import at.huber.youtubeExtractor.VideoMeta
-import at.huber.youtubeExtractor.YouTubeExtractor
-import at.huber.youtubeExtractor.YtFile
 import com.example.videoplayer.databinding.ActivityMainBinding
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import java.net.URL
+import com.google.android.youtube.player.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var simpleExoplayer: SimpleExoPlayer
-    private lateinit var url:Uri
+    private lateinit var listener : YouTubePlayer.OnInitializedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initialiMediaPlayer()
-        extractYoutubeUrl()
 
-    }
+        listener = object : YouTubePlayer.OnInitializedListener{
 
-    private fun initialiMediaPlayer() {
-        simpleExoplayer = SimpleExoPlayer.Builder(this).build()
-        binding.videoPlayer.player = simpleExoplayer
-        buildMediaSource()?.let {
-            simpleExoplayer.prepare(it)
+             override fun onInitializationSuccess(
+                 provider: YouTubePlayer.Provider?,
+                 youTubePlayer: YouTubePlayer,
+                 bool: Boolean
+             ) {
+                 youTubePlayer.cueVideo("W4hTJybfU7s")
+
+             }
+
+             override fun onInitializationFailure(
+                 provider: YouTubePlayer.Provider?,
+                 result: YouTubeInitializationResult?
+             ) {
+
+             }
+
         }
-    }
 
-    private fun buildMediaSource():MediaSource? {
-        val dataSourceFactory = DefaultDataSourceFactory(this,"sample")
-        return ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
-               Uri.parse("https://www.youtube.com/watch?v=lTRiuFIWV54" ))
+
+
     }
 
     override fun onResume() {
+        initializePlayer()
         super.onResume()
-        simpleExoplayer.playWhenReady=  true
+
+    }
+
+    private fun initializePlayer() {
+        binding.video.initialize(YoutubeConfig.getAPIKey(),listener)
     }
 
     override fun onStop() {
         super.onStop()
-        simpleExoplayer.playWhenReady = false
-        if(isFinishing){
-            releasePlayer()
-        }
-    }
-
-    private fun releasePlayer() {
-        simpleExoplayer.release()
-
-    }
-
-    private fun extractYoutubeUrl(){
-        @SuppressLint("StaticFieldLeak")
-        val extracter = object:YouTubeExtractor(this){
-            override fun onExtractionComplete(
-                ytFiles: SparseArray<YtFile>?,
-                videoMeta: VideoMeta?
-            ) {
-
-                val str = ytFiles
-                Uri.parse(ytFiles?.get(22)?.url)
-            }
-
-        }
-        extracter.extract("https://www.youtube.com/watch?v=inOugVa_NYA",true,true)
-
-
-
     }
 
 
