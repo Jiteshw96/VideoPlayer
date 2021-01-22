bc7292cef32d0ecf6bc603e5787e9b82ea46941b
Jiteshw96 <Jitesh.wadhwa13@gmail.com>
2021-01-23 00:25:29 +0530

    "Videos Added"

--- app/src/main/java/com/example/videoplayer/MainActivity.kt
+++ app/src/main/java/com/example/videoplayer/MainActivity.kt
@@ -2,15 +2,13 @@ package com.example.videoplayer
 
 
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.videoplayer.databinding.ActivityMainBinding
 import com.example.videoplayer.databinding.BottomSheetCommentsBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
@@ -32,7 +30,7 @@ class MainActivity : YouTubeBaseActivity() {
                  youTubePlayer: YouTubePlayer,
                  bool: Boolean
              ) {
-                 youTubePlayer.cueVideo("BTYAsjAVa3I")
+                 youTubePlayer.cueVideo("NMFGuy6TRqk")
 
              }
             override fun onInitializationFailure(
@@ -46,34 +44,36 @@ class MainActivity : YouTubeBaseActivity() {
 
        setRecyclerView()
        setUpBottomSheet()
-
     }
+
 
     private fun setUpBottomSheet() {
        // val includedView: View = binding.include.comments
         binding.include.commentBtn.setOnClickListener {
             val bottomSheetDialog:BottomSheetDialog = BottomSheetDialog(this)
             val bottomView = layoutInflater.inflate(R.layout.bottom_sheet_comments,null)
-            val binding = BottomSheetCommentsBinding.inflate(layoutInflater,bottomView as ViewGroup ,false)
-            bottomSheetDialog.setContentView(binding.root)
-            val list = listOf("z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA")
+            val bottomSheetBinding = BottomSheetCommentsBinding.inflate(layoutInflater,bottomView as ViewGroup ,false)
+            bottomSheetDialog.setContentView(bottomSheetBinding.root)
+            val list = listOf("BwuLxPH8IDs","4qq0GQPkfjI","4qq0GQPkfjI","BOHK_w09pVA","qc_QNQzMSCE","o-ins1nvbDg")
             val commentAdapter =  CommentListAdapter(list)
-            binding.commentList.layoutManager = LinearLayoutManager(this)
-            binding.commentList.adapter = commentAdapter
-            binding.commentList.setHasFixedSize(true)
+            bottomSheetBinding.commentList.layoutManager = LinearLayoutManager(this)
+            bottomSheetBinding.commentList.adapter = commentAdapter
+            bottomSheetBinding.commentList.setHasFixedSize(true)
             val dividerItemDecoration = DividerItemDecoration(
-                binding.commentList.context,
+                bottomSheetBinding.commentList.context,
                 LinearLayoutManager.VERTICAL
             )
-            binding.commentList.addItemDecoration(dividerItemDecoration)
+            bottomSheetBinding.commentList.addItemDecoration(dividerItemDecoration)
+            bottomSheetBinding.commentBtn.setOnClickListener {
+                bottomSheetDialog.hide()
+            }
             bottomSheetDialog.show()
-
 
         }
     }
 
     private fun setRecyclerView() {
-        val list = listOf("z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA","z44CLCafepA")
+        val list = listOf("BwuLxPH8IDs","4qq0GQPkfjI","FrteWKKVyzI","BOHK_w09pVA","qc_QNQzMSCE","o-ins1nvbDg")
         val recyclerViewAdapter  = RecyclerViewAdapter(list)
         binding.recyclerView.layoutManager = GridLayoutManager(this,2)
         binding.recyclerView.adapter = recyclerViewAdapter
